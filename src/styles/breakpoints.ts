7f0e1d136816e7d129979a26ad9cbc0283e2634e
alisov66 <alisovk66@gmail.com>
2026-06-11 11:37:54 +0200

    update: grid and spacing added

---
+++ src/styles/breakpoints.ts
@@ -0,0 +1,7 @@
+export const breakpoints = {
+  mobile: 390,
+  tablet: 768,
+  laptop: 1280,
+  desktop: 1440,
+  desktopXL: 1728,
+} as const;
---
+++ src/styles/grid.ts
@@ -0,0 +1,9 @@
+export const grid = {
+  desktop: {
+    width: 1728,
+    columns: 12,
+    margin: 120,
+    gutter: 20,
+    contentWidth: 1488,
+  },
+} as const;
